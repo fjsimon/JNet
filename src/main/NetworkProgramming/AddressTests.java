0e1da12eaf69d2d81b958a653affc6409861d4f0
fjsimon <fjlopez.mail@gmail.com>
2021-05-21 13:57:20 +0100

    Modified AddressTests

--- src/main/NetworkProgramming/AddressTests.java
+++ src/main/NetworkProgramming/AddressTests.java
@@ -14,7 +14,6 @@ public class AddressTests {
     public static void main(String[] args) {
         try {
             InetAddress ip = InetAddress.getByName("www.google.co.uk");
-
             System.out.printf("Host Name %s%n", ip.getHostName());
             System.out.printf("IP Address %s%n", ip.getHostAddress());
             System.out.printf("Version %s%n", getVersion(ip));
