f1702e4e437a67cf26360f2b452e182699c80586
fjsimon <fjlopez.mail@gmail.com>
2025-12-08 19:43:25 +0000

    Added NetworkInterfaceLocal

--- src/com/fjsimon/java/network/programming/NetworkInterfaceLocal.java
+++ src/com/fjsimon/java/network/programming/NetworkInterfaceLocal.java
@@ -12,13 +12,27 @@ public class NetworkInterfaceLocal {
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
             while(interfaces.hasMoreElements()) {
-                NetworkInterface intf = interfaces.nextElement();
-                System.out.println("NetworkInterface display name: " + intf.getDisplayName());
-                Enumeration<InetAddress> addresses = intf.getInetAddresses();
+                NetworkInterface networkInterface = interfaces.nextElement();
+
+                String name = networkInterface.getName();
+                System.out.println("Name:"+name);
+
+                String displayName = networkInterface.getDisplayName();
+                System.out.println("Display name: "+displayName);
+
+                int index = networkInterface.getIndex();
+                System.out.println("Index: " + index);
+
+                int hashCode = networkInterface.hashCode();
+                System.out.println("Hashcode: "+ hashCode);
+
+                System.out.println("\nInet address:");
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
                 while(addresses.hasMoreElements()) {
                     InetAddress address = addresses.nextElement();
                     System.out.println("InitAddress host address: " + address.getHostAddress());
                 }
+                System.out.println("\n");
             }
         } catch (SocketException e) {
             throw new RuntimeException(e);
